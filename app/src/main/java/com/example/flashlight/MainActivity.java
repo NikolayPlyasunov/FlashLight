b43f91b4746e1abe183f69bd31f9730c5d207dcb
Nikolay <svmusic174@gmail.com>
2023-01-26 22:36:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.flashlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bFlash;
+    private FlashClass flashClass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+    private void init(){
+       bFlash = findViewById(R.id.b1);
+       flashClass = new FlashClass(this);
+    }
+    public void onClickFlash(View view){
+        if (flashClass.isFlash_status()){
+            flashClass.flashOff();
+            bFlash.setText("ON");
+            bFlash.setBackgroundResource(R.drawable.circle_green);
+        }
+        else {
+            flashClass.flashOn();
+            bFlash.setText("OFF");
+            bFlash.setBackgroundResource(R.drawable.circle_red);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (flashClass.isFlash_status()){
+            flashClass.flashOff();
+        }
+    }
+}
